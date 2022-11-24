47752ec6b8c6f67ff1710929fa73797e4c1dcb0d
Andrew Leck <Aleck360@gmail.com>
2022-11-23 21:51:44 -0600

    completed section 05_01

--- Ex_Files_Programming_Foundations_Fundamentals/Exercise Files/Chap05/05_01_begin.py
+++ Ex_Files_Programming_Foundations_Fundamentals/Exercise Files/Chap05/05_01_begin.py
@@ -1,5 +1,9 @@
 print("~~~ The Shimmy ~~~")
 
-print("Take one step to the right and stomp.")
-print("Take one step to the left and stomp.")
-print("Shake those hips!") 
+def shimmy():
+    print("Take one step to the right and stomp.")
+    print("Take one step to the left and stomp.")
+    print("Shake those hips!") 
+shimmy()
+shimmy()
+shimmy()
