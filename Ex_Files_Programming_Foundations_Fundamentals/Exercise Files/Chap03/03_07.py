e5cc0e95509437b25aa3df89f1180b67d0a32b3e
Andrew Leck <Aleck360@gmail.com>
2022-11-23 20:58:18 -0600

    Completed section 03_07

--- Ex_Files_Programming_Foundations_Fundamentals/Exercise Files/Chap03/03_07.py
+++ Ex_Files_Programming_Foundations_Fundamentals/Exercise Files/Chap03/03_07.py
@@ -5,10 +5,12 @@ message = "This is going to be tricky ;)"
 Message = "Very tricky!"
 print(message) # show the message on the screen
 
+# answer will be "THis is going to be tricky ;)"
+
 # Perform mathematical operations
-result = 2**3
-print("2**3 =", result)
-result = 5 - 3
+result = 2**3 # = 8
+print("2**3 =", result) # = 8
+result = 5 - 3 # = 2
 #print("5 - 3 =", result)
 
 print("Challenge complete!")
